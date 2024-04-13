e3d41649752dec61572e2394618bf521988482bc
Kaike Medeiros Dourado <116411273+KaikeDourado@users.noreply.github.com>
2024-04-13 10:31:43 -0300

    Delete app/todos directory

--- app/todos/search-todos.tsx
+++
@@ -1,9 +0,0 @@
-export function SearchTodos() {
-  return (
-    <input
-      type="search"
-      placeholder="Pesquisar todo..." 
-      className="block w-80 p-2 mb-4 border rounded-md" 
-    />
-  )
-}
